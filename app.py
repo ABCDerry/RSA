2ec4a9c516e8a7bac04502e536f33187fbb3e113
ABCDerry <168010494+ABCDerry@users.noreply.github.com>
2025-04-05 14:56:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -199,4 +199,6 @@ def decrypt():
         return jsonify({'error': 'Failed to decrypt: ' + str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
